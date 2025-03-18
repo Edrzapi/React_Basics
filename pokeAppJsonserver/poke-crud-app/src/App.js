a9eabc6addb32eb7a09bf44e935e8a39bcd2386a
edrzapi <edrzapi@gmail.com>
2025-03-18 14:47:14 +0000

    updated title and description

--- pokeAppJsonserver/poke-crud-app/src/App.js
+++ pokeAppJsonserver/poke-crud-app/src/App.js
@@ -5,7 +5,6 @@ const App = () => {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Pokémon CRUD App</h1>
-      
       {/* Rendering the PokeList component which handles the actual CRUD operations */}
       <PokeList />
     </div>
